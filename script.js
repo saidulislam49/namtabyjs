70a3233a273a0245a3840589b9df1675d0e5e5ad
Saidul islam <saidulislam49du@gmail.com>
2021-02-23 15:43:02 +0600

    Odd & Even number

---
+++ script.js
@@ -0,0 +1,16 @@
+// function dataPass(){
+//     var inputedValueFirst = Number(document.getElementById('input-1').value) 
+//     var inputedValueSecond = Number(document.getElementById('input-2').value)
+//     var result = 0
+//     for(var i = inputedValueFirst; i <= inputedValueSecond; i ++){
+        
+//         if(i % 2 == 0){
+//             result += '<span>' + i + '</span>'
+//         } 
+         
+//     } 
+
+//     document.getElementById('heading').innerHTML = result
+
+
+// }
